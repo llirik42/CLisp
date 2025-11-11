6d9360e68fb604e557f131b42c419e4a3b7a6627
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-11-11 19:16:22 +0700

    Added reading an AST from stdin

--- translator/src/main.py
+++ translator/src/main.py
@@ -4,7 +4,7 @@ import argparse
 from ast_visitor import ASTVisitor
 from code_rendering import CodeCreator
 from procedure_table import ProcedureTable
-from ast_reading import read_ast
+from ast_reading import read_ast_file, read_ast_stdin
 
 
 def write_generated_code(output_file: str, code: str) -> None:
@@ -22,24 +22,26 @@ def main():
     parser = argparse.ArgumentParser(
         prog="CLisp translator", description="Generates C-code from Lisp-code."
     )
-    parser.add_argument("input_file")
-    parser.add_argument("-o", "--output-file", required=False)
-    parser.add_argument("-p", "--procedure-table", required=False)
-    parser.add_argument("-t", "--templates", required=False)
+    input_group = parser.add_mutually_exclusive_group(required=True)
+    input_group.add_argument(
+        "-i", "--input_stdin", action="store_true", help="Read Lisp-code from stdin"
+    )
+    input_group.add_argument(
+        "-f", "--input_file", action="store", help="Read Lisp-Code from the file"
+    )
+    parser.add_argument("-o", "--output-file", default="output.c")
+    parser.add_argument("-p", "--procedure-table", default="procedure_table.json")
+    parser.add_argument("-t", "--templates", default="code_templates")
     args = parser.parse_args()
 
-    input_file_path = args.input_file
-    output_file_path = args.output_file or "output.c"
-    procedure_table_path = args.procedure_table or "procedure_table.json"
-    templates_folder_path = args.templates or "code_templates"
+    ast = read_ast_stdin() if args.input_stdin else read_ast_file(args.input_file)
 
-    ast = read_ast(input_file_path)
-    code_creator = CodeCreator(templates_folder_path)
-    procedure_table = ProcedureTable(procedure_table_path)
+    code_creator = CodeCreator(args.templates)
+    procedure_table = ProcedureTable(args.procedure_table)
     visitor = ASTVisitor(procedure_table, code_creator)
 
     output_code = visitor.visit(ast)
-    write_generated_code(output_file_path, output_code)
+    write_generated_code(args.output_file, output_code)
 
 
 if __name__ == "__main__":
