783481d87287743a3e43b27018aa19442fa30aef
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-11-12 23:14:51 +0700

    fixes

--- translator/code_templates/function_call.c
+++ translator/code_templates/function_call.c
@@ -1,2 +1,2 @@
 {% set argList = var+'_args' %}Object* {{ argList }}[] = {% raw %}{{% endraw %}{{ args | join(', ') }}{% raw %}}{% endraw %};
-Object* {{ var }} = {{ function }}({{ argList }}, {{ args | length }});
+Object* {{ var }} = {{ function }}({{ args | length }}, {{ argList }});
