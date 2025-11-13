e69d4d9e974d068b3f5d41aa7fd2c1f78cfcf60a
Wooshey1411 <vad2003.barnaul@gmail.com>
2025-11-13 22:41:26 +0700

    Make display depending on the types. Add test for constants.

--- runtime/lib/io.c
+++ runtime/lib/io.c
@@ -8,9 +8,33 @@
 #include "utils.h"
 
 Object* clisp_display(CLISP_FUNC_PARAMS) {
-
     CHECK_FUNC_ARGUMENTS_COUNT(count, 1, EQUAL);
 
-    printf("%d\n", get_int_value(evaluate(args[0])));
+    Object* to_display = evaluate(args[0]);
+
+    switch (get_object_type(to_display)) {
+        case INTEGER:
+            printf("%d\n", get_int_value(to_display));
+            break;
+        case DOUBLE:
+            printf("%f\n", get_double_value(to_display));
+            break;
+        case STRING:
+            printf("%s\n", get_string_value(to_display));
+            break;
+        case BOOLEAN:
+            if (get_boolean_value(to_display)) {
+                printf("true\n");
+            } else {
+                printf("false\n");
+            }
+            break;
+        case UNSPECIFIED:
+            printf("null\n");
+            break;
+        default:
+            printf("Undisplayable type\n");
+    }
+
     return make_unspecified();
 }
