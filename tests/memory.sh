3923f67e9de366ffc1ef6933766860a66242d37a
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-02 16:32:44 +0700

    Added bash-script for checking memory leaks

---
+++ tests/memory.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+
+export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
+
+for file in $(find . -type f -name "*.out"); do
+    if ! valgrind --leak-check=full --error-exitcode=1 "$file" > /dev/null 2>&1; then
+        echo -e "${RED}Memory leak in $file${NC}"
+    else
+        echo -e "${GREEN}No memory leak in $file${NC}"
+    fi
+done
