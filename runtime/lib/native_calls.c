efa445b348bca12934e01dd8a541d16079f4a910
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-04 23:37:22 +0700

    fixed cl_native() so install.sh doesn't fail

--- runtime/lib/native_calls.c
+++ runtime/lib/native_calls.c
@@ -1,6 +1,8 @@
 #include "native_calls.h"
+#include <stdio.h>
 
 CL_Object* cl_native(char* func, char* library, enum CL_NativeType result_type, unsigned int count, CL_NativeArgument* args) {
     // TODO: implement
+    printf("%s-%s-%d-%d-%p\n", func, library, result_type, count, (void*)args);
     return cl_make_unspecified();
 }
