602842ef5fc6f654bf79ef04fb3b1ce90200beb4
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-05 12:56:13 +0700

    native call: varn_args -> varn_args_types

--- translator/code_templates/native_call.c
+++ translator/code_templates/native_call.c
@@ -1,2 +1,2 @@
-{% set args_var = var+'_args' %}{{ arg_type }} {{ args_var }}[] = {% raw %}{{% endraw %}{{ args_types | join(', ') }}{% raw %}}{% endraw %};
+{% set args_var = var+'_args_types' %}{{ arg_type }} {{ args_var }}[] = {% raw %}{{% endraw %}{{ args_types | join(', ') }}{% raw %}}{% endraw %};
 {{ type }} {{ var }} = {{ calling_func }}({{ function }}, {{ library }}, {{result_type}}, {{ args_types | length }}, {{ args_var }});
