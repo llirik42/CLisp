81bcf01ce3a3ae306d133ac068e063c788f5dad5
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-06 13:58:06 +0700

    Fixed templates of the native call

--- translator/code_templates/native_call.c
+++ translator/code_templates/native_call.c
@@ -1,1 +1,2 @@
-{{ type }} {{ var }} = {{ calling_func }}({{ function }}, {{ library }}, {{result_type}}, {{ args_types | length }}, {{ args_types | join(', ') }});
+{% set _args_types = [args_types | length] + args_types %}
+{{ type }} {{ var }} = {{ calling_func }}({{ function }}, {{ library }}, {{result_type}}, {{ _args_types | join(', ') }});
