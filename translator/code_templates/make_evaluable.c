43049584e031d929a92e23c12c1bafedbd2dd738
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-06 12:25:09 +0700

    Removed unused template make_evaluable.c

--- translator/code_templates/make_evaluable.c
+++
@@ -1,2 +0,0 @@
-{% set args_var = var+'_args' %}{{ type }} {{ args_var }}[] = {% raw %}{{% endraw %}{{ args | join(', ') }}{% raw %}}{% endraw %};
-{{ type }} {{ var }} = {{ creation_func }}({{ func }}, {{ args | length }}, {{ args_var }});
