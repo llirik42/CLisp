5db1e918c6d0005f5edaa7d85866021291195796
Kirill Kondrenko <kondrenkokp@yandex.ru>
2025-12-06 12:37:14 +0700

    Made cl_native() with variadic arguments

--- runtime/lib/native_calls.c
+++ runtime/lib/native_calls.c
@@ -1,8 +1,20 @@
 #include "native_calls.h"
 #include <stdio.h>
+#include <stdarg.h>
 
-CL_Object* cl_native(const char* func, const char* library, enum CL_NativeType result_type, unsigned int count, enum CL_NativeType* args_types) {
+CL_Object* cl_native(const char* func, const char* library, enum CL_NativeType result_type, unsigned int count, ...) {
     // TODO: implement (return lambda)
-    printf("%s-%s-%d-%d-%p\n", func, library, result_type, count, (void*)args_types);
+
+    printf("%s-%s-%d-%d\n", func, library, result_type, count);
+
+    va_list args;
+    va_start(args, count);
+
+    for (unsigned int i = 0; i < count; i++) {
+        enum CL_NativeType type = va_arg(args, enum CL_NativeType);
+        printf("%d ", type);
+    }
+    printf("\n");
+
     return cl_make_unspecified();
 }
--- translator/code_templates/native_call.c
+++ translator/code_templates/native_call.c
@@ -1,2 +1,1 @@
-{% set args_var = var+'_args_types' %}{{ arg_type }} {{ args_var }}[] = {% raw %}{{% endraw %}{{ args_types | join(', ') }}{% raw %}}{% endraw %};
-{{ type }} {{ var }} = {{ calling_func }}({{ function }}, {{ library }}, {{result_type}}, {{ args_types | length }}, {{ args_var }});
+{{ type }} {{ var }} = {{ calling_func }}({{ function }}, {{ library }}, {{result_type}}, {{ args_types | length }}, {{ args_types | join(', ') }});
